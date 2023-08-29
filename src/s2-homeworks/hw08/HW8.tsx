42368f94d96dc367038db7f6ff15e2f6a9e339f9
scoomp <moshnyakov.alexei@yandex.ru>
2023-08-29 17:09:40 +0300

    1 commit

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -5,6 +5,9 @@ import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import User from './User'
 
+
+
+
 /*
 * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
 * 2 - дописать компоненту User
