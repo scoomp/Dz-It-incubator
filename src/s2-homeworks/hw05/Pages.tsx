7447d409914b79b974b16564a5907bb9fa964320
scoomp <moshnyakov.alexei@yandex.ru>
2023-10-02 21:22:58 +0300

    10 homeWork /1.3

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -21,7 +21,6 @@ function Pages() {
                 <Route path={'/junior'} element={<Junior/>}/>
                 <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
-
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 <Route path={'*'} element={<Error404/>}/>
             </Routes>
